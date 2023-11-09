a32d308b3397dbe3015c1bfaa71057de6f5d56d5
Soylatte <kksoyeon61@gmail.com>
2023-11-09 16:39:27 +0900

    get API

--- simple/src/App.js
+++ simple/src/App.js
@@ -1,12 +1,31 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList.js';
-import Lifecycle from './Lifecycle.js';
+
+// https://jsonplaceholder.typicode.com/comments
 
 const App = () => {
   const [data,setData] = useState([]);
   const dataId = useRef(0);
+
+  const getData =async() =>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res)=>res.json());
+    const initData = res.slice(0,20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random()*5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++
+      }
+    });
+    setData(initData);
+
+  }
+  useEffect(()=>{
+    getData();
+  },[]);
 
   const onCreate = (author,content,emotion) =>{
     const created_date = new Date().getTime();
@@ -35,7 +54,6 @@ const App = () => {
   };
   return (
     <div className="App">
-      <Lifecycle/>
       <DiaryEditor onCreate={onCreate}/>
       <DiaryList onEdit={onEdit} onDelete={onDelete} diaryList={data}/>
     </div>
